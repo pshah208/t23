d6c82ef5e91b394051eabdd9a08886706a340e46
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-31 16:05:58 -0400

    Update ytubeqa.py

--- ytubeqa.py
+++ ytubeqa.py
@@ -16,7 +16,7 @@ from googletrans import Translator
 
 load_dotenv(find_dotenv())
 # Get an OpenAI API Key before continuing
-OPENAI_API_KEY = os.getenv["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
 llm = ChatOpenAI()
 
